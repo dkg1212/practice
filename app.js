4f21368e7adec824e5523e9176d1527bd4dc49c3
Dimpal Gogoi <dimpalgogoi@Dimpals-MacBook-Air.local>
2025-02-24 22:09:20 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,15 @@
+const heading=React.createElement("div",{},[
+    React.createElement("div",{},[
+        React.createElement("h1",{},"Helloworld From React"),
+        React.createElement("h2",{},"Helloworld From React")
+    ]),
+    React.createElement("div",{},[
+        React.createElement("h1",{},"Helloworld From React"),
+        React.createElement("h2",{},"Helloworld From React")
+    ])
+
+]);
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
