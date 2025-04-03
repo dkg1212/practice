1c272cab829c0b1b1cfdaad5b00108ca77742994
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-04-03 22:29:04 +0530

    add Heading onto app.js and also static body

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title =(
-<h1 className="head" tabIndex="5">
-    "Namastay React🚀"
-    </h1>
-);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ *      - Img
+ *      - Name of Res, Star Rating, cuisine, delery tie
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
 
-const HeadingComponent =()=>(
-    <div id="container">
-        {Title}
-        <h1 className="heading">Namastay React From Functional component 🚀 </h1>
+const Header=()=>{
+    return(
+    <div className="header">
+        <div className="logo-coniner">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+
     </div>
-);
+    );
+};
+const RestaurantCard=()=>{
+    return (
+        <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+            <img 
+            className="res-logo"
+            alt="res-logo"
+            src="https://b2958125.smushcdn.com/2958125/wp-content/uploads/Chettinadu-Chicken-Biryani-2.png?lossy=1&strip=1&webp=1"
+            />
+            <h3>foods</h3>
+            <h4>Biryani ,North Indian, Asain </h4>
+            <h4>4.4</h4>
+            <h4>38</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )}
+const AppLayout=()=>{
+    return(
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
