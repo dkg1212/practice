a14fa088b992665046b4d567cc6c0838e5712690
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-04-07 23:45:26 +0530

    add shimmer

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,31 @@
-import RestaurantCard from "./ResturantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import RestaurantCard from "./RestaurantCard";
+import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-    const [listOfResturants,setListOfResturant]=useState(resList);
-    return (
+    const [listOfResturants,setListOfResturant]=useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            //"https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=26.63510&lng=92.80300&carousel=true&third_party_vendor=1"
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+        );
+    
+        const json = await data.json();
+        console.log(json)
+    
+        // Optional Chaining
+        setListOfResturant(json?.data?.cards[2]?.data?.data?.cards);
+    };
+
+    return listOfResturants.length === 0 ? (
+        <Shimmer />
+      ) :(
       <div className="body">
         <div className="filter">
             <button 
